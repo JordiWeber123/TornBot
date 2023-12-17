431649fc43d49de47dac0537a4c5a41f555d6cb1
Jordi <misterreco099@gmail.com>
2023-12-16 23:28:48 -0600

    Completed most functionality for bot Forgot to make small commits (lol)

--- price_bot.py
+++ price_bot.py
@@ -1,7 +1,7 @@
 from torn_client import User as TornUser
 from time import sleep
 import discord as ds
-from discord.ext import commands
+from discord.ext import tasks, commands
 from discord import User as DiscordUser
 import json
 
@@ -16,35 +16,83 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     #await tree.sync()#guild=discord.Object(id=my guild id)
     print("Hello, I am MisterReco's Torn Bot")
+    check_prices.start()
 
 #add a user given their Torn API Key
 @bot.command()
 async def add_user(ctx, key):
-    user = str(ctx.author)
+    user = str(ctx.author.id)
     if user not in users:
         users[user] = TornUser(key)
-        await ctx.send(f"User {user} has been added")
+        await ctx.send(f"User @{ctx.author.id} has been added")
     else:
-        await ctx.send(f"User {user} already added")
+        await ctx.send(f"User @{ctx.author.id} already added")
 #add an item to track
 @bot.command()
 async def add(ctx, id, threshold):
-    user = ctx.author
+    user = str(ctx.author.id)
+    id = int(id)
     threshold = int(threshold)
-    if str(user) not in users:
-        await ctx.send(f"User {user} not in data base, use command `add_user`")
+    if user not in users:
+        await ctx.send(f"User @{ctx.author.id} not in data base, use command `add_user`")
     else:
+        users[user].update_threshold(id, threshold)
         await ctx.send(f"Adding item #{id} with a threshold of ${threshold:,}")
 
 #update an item currently being tracked
 @bot.command()
 async def update(ctx, id, threshold):
-    await ctx.send(f"Updating item #{id} with a threshold of {threshold}")
+    user = str(ctx.author.id)
+    threshold = int(threshold)
+    id = int(id)
+    if user not in users:
+        await ctx.send(f"User @{ctx.author.id} not in data base, use command `add_user`")
+    else:
+        users[user].update_threshold(id, threshold)
+        await ctx.send(f"Updating item #{id} with a threshold of {threshold}")
 
 #remove an item from being tracked
 @bot.command()
 async def remove(ctx, id):
-    await ctx.send(f"Stopped tracking item #{id}")
+    id = int(id)
+    user = str(ctx.author.id)
+    if await check_user(ctx,user):
+        torn_user = users[user]
+        if torn_user.check_added(id):
+            torn_user.remove(id)
+            await ctx.send(f"Stopped tracking item #{id}")
+        else:
+            await ctx.send(f"Item #{id} is not currently being tracked")
+@bot.command()
+async def list_items(ctx):
+    user = str(ctx.author.id)
+    if await check_user(ctx, user):
+        await ctx.send(f"These are the items user @{user} is currently tracking")
+        list_items = "ID \tThreshold"
+        listed_items += users[user].list_items()
+        #TODO Make an embed instead of send
+        await ctx.send(listed_items)
+@bot.command()
+async def dm(ctx):
+    channel = await bot.create_dm(ds.Object(235894085922193408))
+    await channel.send("This is a dm")
+async def check_user(ctx, user_id):
+    if user_id not in users:
+        await ctx.send(f"User @{ctx.author.id} not in data base, use command `add_user`")
+        return False
+    return True
+
+
+
+@tasks.loop(seconds=30)
+async def check_prices():
+    for id, torn_user in users.items(): 
+        print(f"Checking key {torn_user.key}")
+        message = (torn_user.check_items())
+        if len(message) > 0:
+            channel = await bot.create_dm(ds.Object(int(id)))
+            await channel.send(message)
+
 
 while True:
     try:
@@ -52,6 +100,6 @@ while True:
         bot.run(TOKEN)
     except Exception:
         print("Exception")
-        #with open("users.json", "w") as fp:
-        #    json.dump(users, fp)
+        with open("users.json", "w") as fp:
+            json.dump(users, fp)
         sleep(70)
--- torn_client.py
+++ torn_client.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 base = "https://api.torn.com/market/"
 torn_store_base = "https://www.torn.com/imarket.php#/p=shop&step=shop&type=&searchname="
 bazaar_ending = "?selections=bazaar&key="
@@ -9,18 +9,25 @@ torn_items = "https://api.torn.com/torn/?selections=items&key="
 BAZAAR_INDEX = 0
 MARKET_INDEX = 1
 
-class User:
+class User(dict):
     def __init__(self, key):
-        self.key = key
-        self.item_dict = {}
+        if type(key) is dict:
+            self.key = str(key["key"])
+            self.item_dict = dict(key["item_dict"])
+        else:
+            self.key = key
+            self.item_dict = {}
+            dict.__init__(self, key=key, item_dict=self.item_dict)
         
-    #For getting from the request tuple
-
+    def __repr__(self):
+        return f"{self.key} {self.item_dict}" 
+    
     def get_request_urls(self, id: int):
         item_str = str(id)
         return (base + item_str + bazaar_ending + self.key, base + item_str + market_ending + self.key)
     #Checks all the items in the items_dict, if any has 
     def check_items(self):
+        message = ""
         torn_item_dict = requests.get(torn_items + self.key).json()["items"]
         for id, threshold in self.item_dict.items():
             urls = self.get_request_urls(id)
@@ -32,23 +39,37 @@ class User:
                     item_name = torn_item_dict[str(id)]["name"]
                     cost = listing_info["cost"]
                     quantity = listing_info["quantity"]
-                    print(f"{item_name}: ${cost:,} x{quantity:,} = ${cost*quantity:,}")
-                    print(torn_store_base + item_name.replace(" ", "+"))
+                    message += (f"{item_name}: ${cost:,} x{quantity:,} = ${cost*quantity:,}\n")
+                    message += (torn_store_base + item_name.replace(" ", "+") + "\n")
                     break
+        return message
 
     #Maybe unnecessary tbh
     def update_threshold(self, item_id: int, threshold: int):
-        self.item_dict[id] = threshold
+        self.item_dict[item_id] = threshold
 
     #Maybe unnecessary tbh
     def check_added(self, item_id: int) -> bool:
         return item_id in self.item_dict
+    
+    def remove(self, id):
+        del self.item_dict[id]
 
+    def list_items(self):
+        return "".join([f"{key}\t ${value:,}\n" for key, value in self.item_dict.items()])
 def test():
     tst_user = User("L1ZLIuYKllusUNts")
     tst_user.item_dict[367] = 13_800_000
     tst_user.item_dict[283] = 24_000_000
-    tst_user.check_items()
+    #print(tst_user.check_items())
+    print(tst_user.list_items())
+    dump = json.dumps(tst_user) 
+    print(json.dumps(tst_user), type(dump))
+    loaded = json.loads(dump)
+    print(loaded, type(loaded))
+    reconstructed = User(loaded)
+    print(reconstructed, type(reconstructed))
+
 
 if __name__ == "__main__":
     test()
